3dbca8f282b787ea61fc82f69422373a2fb74bf5
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-11-15 08:58:22 +0100

    feat: unsupported flags indicator

--- shared-compose-settings/src/commonMain/kotlin/dev/giuliopime/shared_compose_settings/pages/SystemPage.kt
+++ shared-compose-settings/src/commonMain/kotlin/dev/giuliopime/shared_compose_settings/pages/SystemPage.kt
@@ -1,9 +1,15 @@
 package dev.giuliopime.shared_compose_settings.pages
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import dev.giuliopime.shared_compose_settings.FeatureFlags
 import dev.giuliopime.shared_compose_settings.components.BooleanSetting
@@ -25,6 +31,19 @@ fun SystemPage(
         enabled = featureFlags.resetOnIdle
     )
 
+    AnimatedVisibility(
+        visible = !featureFlags.resetOnIdle,
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.errorContainer, MaterialTheme.shapes.medium)
+            .padding(horizontal = 12.dp, vertical = 12.dp)
+            .clip(MaterialTheme.shapes.medium)
+    ) {
+        Text(
+            text = "This feature is currently not supported for your operating system!",
+            color = MaterialTheme.colorScheme.onErrorContainer
+        )
+    }
+
     Spacer(Modifier.height(16.dp))
 
     BooleanSetting(
@@ -35,4 +54,17 @@ fun SystemPage(
         name = "Start at login",
         enabled = featureFlags.startAtLogin
     )
+
+    AnimatedVisibility(
+        visible = !featureFlags.startAtLogin,
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.errorContainer, MaterialTheme.shapes.medium)
+            .padding(horizontal = 12.dp, vertical = 12.dp)
+            .clip(MaterialTheme.shapes.medium)
+    ) {
+        Text(
+            text = "This feature is currently not supported for your operating system!",
+            color = MaterialTheme.colorScheme.onErrorContainer
+        )
+    }
 }
