dec62a3d1295f5ffbc53e337a26ce1c898c0df66
runner <runner@Mac-1725223972442.local>
2024-09-01 21:47:18 +0000

    KMM SPM package release for 0.1.1

--- Package.swift
+++ Package.swift
@@ -1,13 +1,16 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/Giuliopime/ZenBreak/dev/giuliopime/zenbreakcorekit/shared-core-kmmbridge/0.1.1/shared-core-kmmbridge-0.1.1.zip"
+let remoteKotlinChecksum = "2f60211aceaba5d94616d420b5e6a59ea013bf91ec8255142f64f057753a7647"
 let packageName = "ZenBreakCoreKit"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
+        .iOS(.v13)
     ],
     products: [
         .library(
@@ -18,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared-core/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
